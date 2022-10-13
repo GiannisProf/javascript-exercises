0d1f93511a6fee8766d09949ef43d7f48f81f2d6
Giannis Profis <giannishandball@gmail.com>
2022-10-14 00:00:55 +0300

    complete test 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1,num2) {
+    if(typeof num1 != "number" || num1 < 0 || typeof num2 != "number" || num2 < 0){return "ERROR"}
+    let max = Math.max(num1,num2);
+    let sum = Math.min(num1,num2);
+    for(let i = sum +1; i <= max; i++){
+        sum+=i;
+    }
+        return sum;
+        
+    }
 
 // Do not edit below this line
 module.exports = sumAll;
