7325c8db9959caf04e36e3e6bffd260e7e8394f0
Giannis Profis <giannishandball@gmail.com>
2022-10-13 23:54:40 +0300

    complete test 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let currentString = "";
+    if(num < 0){return "ERROR";}
+    for(let i = 0; i<num; i++){
+        currentString += string;
+    }
+    return currentString;
 };
 
 // Do not edit below this line
