34a93b6fb8fedd7f2865d5ac7a9f93c0c5f959d2
Giannis Profis <giannishandball@gmail.com>
2022-10-13 23:59:52 +0300

    complete test 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...theArgs){
+    for(let i = 0; i < array.length; i++){  
+        for(let j = 0; j < theArgs.length; j++){
+            if(array[i]===theArgs[j]){
+                array.splice(i,1)
+                i--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
