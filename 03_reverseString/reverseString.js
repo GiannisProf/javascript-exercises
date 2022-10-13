7cfa6d6e9ae1c65b7b463ef5875deac3bf16c041
Giannis Profis <giannishandball@gmail.com>
2022-10-13 23:57:20 +0300

    complete test 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let stringArr = input.split("");
+    let reverseStrArr = stringArr.reverse();
+    let joinedStrArr = reverseStrArr.join("");
+    
+    return joinedStrArr;
 };
 
 // Do not edit below this line
