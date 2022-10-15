e4d2740af790f2f2d48aa69bd879851cf9162045
Giannis Profis <giannishandball@gmail.com>
2022-10-15 20:08:30 +0300

    complete test 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,41 @@
-const add = function() {
-	
+const add = function(number1,number2) {
+  result = +number1 + +number2;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+  let result = number1 - number2;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for(let i = 0; i < array.length; i++){
+    sum += array[i];
+  }
+  return +sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let result = 1;
+  for(let i = 0; i < array.length; i++){
+    result *= array[i];
+  }
+  return result;
 
 };
 
-const power = function() {
-	
+const power = function(number1, number2) {
+	let result = Math.pow(number1,number2);
+  return result;
 };
 
-const factorial = function() {
+const factorial = function(number) {
+  if (number === 0){return 1;}
+  else if (number === 1){return 1;}
+  else{
+    return factorial(number - 1)*number;
+  }
 	
 };
 
