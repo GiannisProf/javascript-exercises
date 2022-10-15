f68b6aee7278b7958b4aaa08462fce5bf2b6750d
Giannis Profis <giannishandball@gmail.com>
2022-10-15 21:38:25 +0300

    complete test 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    input = input.toLowerCase().replace(/[^a-z]/g, "");
+    return input.split("").reverse().join("") == input;
 };
 
 // Do not edit below this line
